e3c75d1cd910936e7eb5f5c5c77af7ade51aed56
arminshami7 <www.arminshami7@gmail.com>
2025-12-24 18:53:05 +0330

    Initial commit: Portfolio project setup complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <header className="bg-gray-800/50 backdrop-blur-sm sticky top-0 z-50">
       <nav className="container mx-auto flex items-center justify-between p-4 text-white">
         {/* بخش لوگو/اسم */}
-        <div className="text-2xl font-bold">
+        <div className="text-2xl font-bold font-mono border-2 rounded-xl px-4 ">
           <a href="#">Armin Sahami</a>
         </div>
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,11 @@ function Hero() {
     return(
         <section id="Home" className="min-h-screen flex items-center justify-center text-center">
             <div className="max-w-3x1">
+                <img src="images/profile1.jpg" className="w-40 mx-auto m-8 bg-gray-800"></img>
                 <h1 className="text-5x1 md:text-7x1 font-extrabold">
-                    <span className="bg-gradiant-to-r from-blue-400 to-teal-300 bg-clip-text text-transparent">
-                        Your Name
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r
+                    from-blue-400 to-teal-300 font-mono text-4xl">
+                        Armin Sahami
                     </span>
                 </h1>
                 <p className="text-2x1 md:text-3x1 text-gray-300 mt-4">
@@ -15,7 +17,8 @@ function Hero() {
                 </p>
                 <a 
                 href="#project"
-                className="mt-8 inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-8 rounded-full ">
+                className="mt-8 inline-block bg-blue-500 hover:bg-blue-600 text-white 
+                font-bold py-3 px-8 rounded-full transition-colors">
                     View My Work
                 </a>
             </div>
